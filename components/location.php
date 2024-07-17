6b0f84df7386f983d9addbe72a55518593bf0993
Harsh-09A <harsh.autowebbed@gmail.com>
2024-07-17 13:08:56 +0530

    Changed Map & Increased Blur

--- components/location.php
+++ components/location.php
@@ -11,7 +11,7 @@
 
         <p class="text-white loc_para">Discover a world of convenience and excitement just moments away from VM Mohan Samah Nerul. From cultural landmarks like Swaminarayan Temple to educational institutions and major transport hubs, everything you need is within easy reach.</p> -->
 
-        <iframe class="iframe_map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3771.493719968956!2d73.00486647712376!3d19.042018307052224!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c3e5c128ef15%3A0x65ca308a835eb560!2s12%2C%20Sector%206%20Nerul%20Rd%2C%20Sarsole%20Gaon%2C%20Sector%206%2C%20Nerul%2C%20Navi%20Mumbai%2C%20Maharashtra%20400706!5e0!3m2!1sen!2sin!4v1721021872059!5m2!1sen!2sin" width="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <iframe class="iframe_map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3771.4579584183043!2d73.0140473!3d19.0435923!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c3942fc7cad3%3A0xca228157c58c0e4c!2sVM%20constructions!5e0!3m2!1sen!2sin!4v1721201673401!5m2!1sen!2sin" width="100%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 
       </div>
       <div class="col-12 col-md-6">
@@ -24,7 +24,7 @@
           </div>
           <div class="loc_bor d-flex justify-content-between align-items-center">
             <p>
-              <i class="fa-solid fa-train"></i>Terna Hospital
+              <i class="fa-solid fa-hospital"></i>Terna Hospital
             </p>
             <span>15 Min</span>
           </div>
